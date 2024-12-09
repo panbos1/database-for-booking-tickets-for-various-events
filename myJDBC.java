63f45ee7184a4625469bcd3b6fc66057110536ed
MariaKakoulidou <mariakak2001@gmail.com>
2024-12-09 22:25:01 +0200

    testt

--- myJDBC.java
+++ myJDBC.java
@@ -1,25 +1,34 @@
 import java.sql.*;
 
-public class myJDBC {
-    public static void main(String[] args) {
-        try {
-            Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/login_scema", "root", "29102001");
+public class MyJDBC {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
 
-            Statement statement = connection.createStatement();
+        String url = new String("jdbc:mysql://localhost");
+        String databaseName = new String("test");
+        int port = 3306;
+        String username = new String("root");
+        String password = new String("");
 
-            String createTable = "CREATE TABLE IF NOT EXISTS USERS (username VARCHAR(50), password VARCHAR(50))";
-            String insertUser1 = "INSERT INTO USERS (username, password) VALUES ('user1', 'pass1')";
+        Connection con = DriverManager.getConnection(url + ":" + port + "/" + databaseName + "?characterEncoding=UTF-8", username, password);
 
-            statement.executeUpdate(createTable);
-            statement.executeUpdate(insertUser1);
+        Statement stmt = con.createStatement();
 
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM USERS");
+        // SQL to create the Users table
+        String createTableSQL = "CREATE TABLE IF NOT EXISTS Users (" +
+                "id INT AUTO_INCREMENT PRIMARY KEY, " +
+                "username VARCHAR(50) NOT NULL, " +
+                "password VARCHAR(100) NOT NULL)";
+        stmt.executeUpdate(createTableSQL);
 
-            while (resultSet.next()) {
-                System.out.println(resultSet.getString("username") + " " + resultSet.getString("password"));
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        String insertUsersSQL = "INSERT INTO Users (username, password) VALUES " +
+                "('user1', 'password1'), " +
+                "('user2', 'password2')";
+        int rowsInserted = stmt.executeUpdate(insertUsersSQL);
+
+        
+        stmt.close();
+        con.close();
+
     }
 }
